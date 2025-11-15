df0fc95738651119d16243dd70d127e269ae49f4
luistoromarin <skill.main.luis@gmail.com>
2025-11-15 10:56:28 -0300

    "ajuste variables"

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,16 +2,15 @@
 export const environment = {
     production: false,
     firebaseConfig: {
-        apiKey: "AIzaSyDxPLpWh_LCBmTSE2gLVn-dNc-EvQrs4fo",
-        authDomain: "fir-angular20-chat.firebaseapp.com",
-        projectId: "fir-angular20-chat",
-        storageBucket: "fir-angular20-chat.firebasestorage.app",
-        messagingSenderId: "82429704495",
-        appId: "1:82429704495:web:877facb7e260db9dccc9da",
-        measurementId: "G-WYB5SG6ETJ"
+        apiKey: "AIzaSyDdM8Cwrek-EakRpoYAjWPLyz7FUOG36nA",
+        authDomain: "skillmain-88467.firebaseapp.com",
+        projectId: "skillmain-88467",
+        storageBucket: "skillmain-88467.firebasestorage.app",
+        messagingSenderId: "689204958847",
+        appId: "1:689204958847:web:aaa4525efb98a8de81914a"
     },
     gemini: {
         apiUrl: "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent",
-        apiKey: "AIzaSyCkcxvOby8Us91YGf-gApZ1V7RvpkVA3-M"
+        apiKey: "AIzaSyBiEbGsXAM7r-mWKpy3bsQy-_yOqKsHoZs"
     }
 }
